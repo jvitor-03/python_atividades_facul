6ec4886fe05712c41ba678c2f73ba426703226f2
João Vitor <jvitor0310@gmail.com>
2026-04-29 16:57:31 -0300

    Atividades sobre somas

---
+++ exercicios.py
@@ -0,0 +1,9 @@
+num1 = int(input('Digite um numero: '))
+num2 = int(input('digite outro numero: '))
+resultado = num1 + num2
+resultado1 = num1 - num2
+resultado2 = num1 * num2
+resultado3 = num1 / num2
+
+print(f'Os resultados da soma são {resultado}, a subtração {resultado1},\n a multiplicação é {resultado2} e a divisão é {resultado3}')
+
