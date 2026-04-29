975f59283e94d48a7359de32e8fa64e26df1ef32
João Vitor <jvitor0310@gmail.com>
2026-04-29 17:10:43 -0300

    Consumo de combústivel, Conta

---
+++ consumo de combustivel.py
@@ -0,0 +1,7 @@
+
+#como determinar o consumo médio de um automóvel
+
+distancia = float(input('Quanto é a distancia total percorrida pelo automóvel?' ))
+combustivel = float(input('Qual é o total de combustível gasto? '))
+conta = distancia / combustivel
+print(f'O seu consumo médio de gasolina é {conta}')
